efd3f134fa76272ddab56f93327642214cbb0712
iut <iut@10.0.2.15>
2018-11-26 15:27:18 +0100

    Fin Session 1

--- js/test.js
+++ js/test.js
@@ -2,6 +2,7 @@
 
 var exampleSocket = new WebSocket("ws://127.0.0.1:8080/chat?pseudo=%27toto%27", "protocolOne");
 
-exampleSocket.onopen = function (event) {
-	  console.log(exampleSocket); 
-	};
+exampleSocket.addEventListener("open",function (event) {
+	console.log("Connecté");
+})
+
